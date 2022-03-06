9ce058d572e56c836727807b156ed2ddad65d76b
thekavikumar <kavikumarceo@gmail.com>
2022-03-06 12:19:36 +0530

    upload

---
+++ app/src/main/java/com/codesharks/logindemo/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.codesharks.logindemo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    var mAuth = FirebaseAuth.getInstance()
+    lateinit var myDB : DatabaseReference
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val signInBtn = findViewById<View>(R.id.sign_in) as Button
+
+        signInBtn.setOnClickListener(View.OnClickListener {
+          view ->   registerUser()
+        })
+    }
+
+    private fun registerUser () {
+        val emailTxt = findViewById<View>(R.id.email) as EditText
+        val passwordTxt = findViewById<View>(R.id.pass) as EditText
+
+        var email = emailTxt.text.toString()
+        var password = passwordTxt.text.toString()
+
+        if (!email.isEmpty() && !password.isEmpty()) {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val user = mAuth.currentUser
+                    val uid = user!!.uid
+                    Toast.makeText(this, "Successfully registered :)", Toast.LENGTH_LONG).show()
+                }else {
+                    Toast.makeText(this, "Error registering, try again later :(", Toast.LENGTH_LONG).show()
+                }
+            })
+        }else {
+            Toast.makeText(this,"Please fill up the Credentials :|", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun login () {
+        val emailTxt = findViewById<View>(R.id.email) as EditText
+        var email = emailTxt.text.toString()
+        val passwordTxt = findViewById<View>(R.id.pass) as EditText
+        var password = passwordTxt.text.toString()
+
+        if (!email.isEmpty() && !password.isEmpty()) {
+            this.mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener ( this, OnCompleteListener<AuthResult> { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Successfully Logged in :)", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Error Logging in :(", Toast.LENGTH_SHORT).show()
+                }
+            })
+
+        }else {
+            Toast.makeText(this, "Please fill up the Credentials :|", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
